0dfb985da514c7bc81d3466b9322db83afdfbcd2
Mica Linscheid <micascheid@gmail.com>
2023-04-08 12:26:59 -0600

    #10, will probably need to make adjustments but good enough to start deploying to firebase hosting and testing out there a bit, I'm exited lol to get this thing out

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,35 +1,80 @@
-import {React, useState} from 'react';
+import {Fragment, React, useState} from 'react';
 import {Button, Stack, TextField, Typography} from "@mui/material";
+import {LoadingButton} from '@mui/lab';
+import { getFunctions, httpsCallable} from "firebase/functions";
+import axios from 'axios';
 
-
-
+const API_BASE_URL = 'http://localhost:5000';
 
 const DefaultDashboard = () => {
     const [podLink, setPodLink] = useState('');
+    const [isLink, setIsLink] = useState(false);
     const [error, setError] = useState(false);
     const [helperText, setHelperText] = useState('');
-
+    const [summary, setSummary] = useState('');
+    const [isActiveButton, setIsActiveButton] = useState(null);
+    const [isSummarizing,setIsSummarizing] = useState(false);
     const handleTextChange = (event) => {
-        setPodLink(event.target.value);
+        const linkVal = event.target.value;
+        setIsLink(!!linkVal);
+        setPodLink(linkVal);
     }
-    const linkSubmitHandler = () => {
-        /*
-        1.) link validation https://podcasts.apple.com/us/podcast/
-        2.) Send request to cloud function
-         */
+
+    const getSummary = async (link, numBulletPoints) => {
+        console.log("trying summary");
+        const data = {
+            podcastEpisodeLink: link,
+            numBulletPoints:numBulletPoints
+        };
+        axios.post("http://127.0.0.1:5000/get_summary", data)
+            .then((response) => {
+                console.log("Episode Name: " +  response.data.transcription);
+                setSummary(response.data.transcription);
+                setIsSummarizing(false);
+                setIsActiveButton(null);
+            })
+            .catch((error) => {
+                console.log("Error:" + error);
+                setIsSummarizing(false);
+                setIsActiveButton(null);
+                setSummary("SOL for right now, we are working on a fix ");
+            });
+    };
+    const linkSubmitHandler = (buttonId) => {
+        setIsActiveButton(buttonId);
+        setIsSummarizing(buttonId);
+        console.log("DATA FROM BUTTON" + buttonId);
         const inputString = podLink;
         const pattern = /^https:\/\/podcasts\.apple\.com\/us\/podcast\//;
-
         const validLink = pattern.test(inputString);
+
         if (validLink){
             setError(false);
-            setHelperText("Getting your summary!")
+            setHelperText("Getting your summary!");
             console.log('summary requested');
+            getSummary(inputString, buttonId);
         } else {
             setError(true);
             setHelperText("Please enter a valid Apple Podcast Link, Spotify coming soon!")
         }
     }
+
+    const newlineToBreak = (text) => {
+        return text.split('\n').map((item, index, array) => (
+            <Fragment key={index}>
+                {item}
+                {index !== array.length-1 && <br />}
+            </Fragment>
+        ))
+    };
+
+    const isDisabled = (buttonId) => {
+        if (!isLink){
+            return true;
+        } else {
+            return isActiveButton !== null && isActiveButton !== buttonId;
+        }
+    };
 
     return (
         <Stack spacing={2}>
@@ -42,8 +87,29 @@ const DefaultDashboard = () => {
                 onChange={handleTextChange}
                 error={error}
                 helperText={helperText}></TextField>
-            <Button variant={"contained"} onClick={linkSubmitHandler}>Summarize!</Button>
-            <Typography variant={"h2"}>Summary:</Typography>
+            {/*<Button variant={"contained"} onClick={linkSubmitHandler}>Summarize!</Button>*/}
+            <Stack direction={"row"} spacing={2} justifyContent={"space-evenly"}>
+                {
+                    Array.from({length: 3}, (_, index) => {
+                        const buttonId = index + 3;
+                        const isLoading = isSummarizing && isActiveButton === buttonId;
+                        return (
+                            <LoadingButton
+                                loading={isLoading}
+                                key={buttonId}
+                                variant={"contained"}
+                                onClick={() => linkSubmitHandler(buttonId)}
+                                disabled={isDisabled(buttonId)}>
+                                {buttonId + " Bullet Points"}
+                            </LoadingButton>
+
+                        )
+                    })
+                }
+            </Stack>
+            {isSummarizing && <Typography variant={"h2"}>Getting Your Bullet Points!</Typography>}
+
+            <Typography variant={"h6"}>{newlineToBreak(summary)}</Typography>
         </Stack>
     );
 };
